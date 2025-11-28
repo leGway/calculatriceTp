988b689e15f43c68505a713060fb05e69991c301
Godwin Ayita <legodway@gmail.com>
2025-11-28 10:55:44 +0100

    Version 2 : Historique et Design final

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,18 +3,84 @@
 
 // Importe le package Material de Flutter pour les widgets de conception Material.
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 // --------------------------------------------------------------------------
 // 1. Widget de la Page d'Accueil (HomePage)
 // --------------------------------------------------------------------------
 
 /// Widget sans état représentant la page principale de la calculatrice.
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   /// Constructeur de la HomePage.
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String expression = "0";
+  String historique = "";
+  void onButtonTap(String valeur) {
+    setState(() {
+      if (expression == "0") {
+        expression = valeur;
+      } else {
+        expression += valeur;
+      }
+    });
+  }
+
+  void resetScreen() {
+    setState(() {
+      expression = "0";
+      historique = "0";
+    });
+  }
+
+  void deleteLast() {
+    setState(() {
+      if (historique.length == 1 || historique == "0") {
+        historique = "0"; // On remet à zéro
+      } else {
+        // Sinon, on coupe le dernier chiffre
+        // Exemple : "123" devient "12"
+        historique = historique.substring(0, historique.length - 1);
+      }
+    });
+  }
+
+  void calculateResult() {
+    String finalInput = expression;
+    finalInput = finalInput.replaceAll('x', '*');
+    finalInput = finalInput.replaceAll('÷', '/');
+
+    try {
+      ShuntingYardParser p = ShuntingYardParser();
+
+      historique = expression;
+
+      Expression exp = p.parse(finalInput);
+
+      ContextModel cm = ContextModel();
+
+      // ignore: deprecated_member_use
+      double eval = exp.evaluate(EvaluationType.REAL, cm);
+
+      setState(() {
+        expression = eval.toString();
+      });
+    } catch (e) {
+      setState(() {
+        expression = "Erreur";
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    const Color grisChiffre = Color(0xFF616161);
+    const Color grisPrincipal = Color(0xFF303030);
     // SafeArea garantit que le contenu n'empiète pas sur les zones du système d'exploitation
     // telles que la barre de notification (encoche, etc.).
     return SafeArea(
@@ -41,17 +107,18 @@ class HomePage extends StatelessWidget {
                 ],
               ),
 
-              // Espace vertical entre l'en-tête et l'affichage principal.
-              const SizedBox(height: 130),
+              // Le Spacer agit comme un ressort. Il pousse le contenu vers le bas
+              // en occupant tout l'espace vide disponible.
+              const Spacer(),
 
               // Deuxième ligne : Affichage du résultat actuel.
               Row(
                 // end pour aligner les enfants à droite (comme sur une calculatrice).
                 mainAxisAlignment: MainAxisAlignment.end,
-                children: const [
+                children: [
                   // Texte du résultat principal (actuellement "0").
                   Text(
-                    "0",
+                    expression,
                     style: TextStyle(color: Colors.white, fontSize: 70),
                   ),
                   // Espace horizontal.
@@ -65,16 +132,22 @@ class HomePage extends StatelessWidget {
               Row(
                 // end pour aligner les enfants à droite.
                 mainAxisAlignment: MainAxisAlignment.end,
-                children: const [
+                children: [
                   // Texte de l'entrée/historique (actuellement "0"), en gris et plus petit.
-                  Text("0", style: TextStyle(color: Colors.grey, fontSize: 30)),
+                  Text(
+                    historique,
+                    style: TextStyle(color: Colors.grey, fontSize: 30),
+                  ),
                   // Espace horizontal.
-                  SizedBox(width: 10),
+                  SizedBox(width: 15),
                   // Icône pour effacer ou supprimer, colorée en orange et plus grande.
-                  Icon(
-                    Icons.keyboard_double_arrow_left_rounded,
-                    color: Colors.orange,
-                    size: 40,
+                  IconButton(
+                    icon: const Icon(
+                      Icons.backspace_outlined,
+                      color: Colors.orange,
+                      size: 30,
+                    ),
+                    onPressed: deleteLast,
                   ),
                 ],
               ),
@@ -83,59 +156,164 @@ class HomePage extends StatelessWidget {
               // Utilise Row pour organiser les colonnes des boutons horizontalement.
               Row(
                 // spaceAround pour répartir l'espace autour des colonnes de boutons.
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   // Première Colonne de boutons
                   Column(
                     children: [
                       // AC (Clear All - Effacer Tout)
-                      const KeyboardCard(),
+                      KeyboardCard(
+                        valeurKey: "AC",
+                        couleurKey: grisPrincipal,
+                        onPressed: resetScreen,
+                      ),
                       // Chiffres et +/-
-                      const KeyboardCard(valeurKey: "7"),
-                      const KeyboardCard(valeurKey: "4"),
-                      const KeyboardCard(valeurKey: "1"),
-                      const KeyboardCard(valeurKey: "+/-"),
+                      KeyboardCard(
+                        valeurKey: "7",
+                        couleurKey: grisChiffre,
+                        onPressed: () {
+                          onButtonTap("7");
+                        },
+                      ),
+                      KeyboardCard(
+                        valeurKey: "4",
+                        couleurKey: grisChiffre,
+                        onPressed: () {
+                          onButtonTap("4");
+                        },
+                      ),
+                      KeyboardCard(
+                        valeurKey: "1",
+                        couleurKey: grisChiffre,
+                        onPressed: () {
+                          onButtonTap("1");
+                        },
+                      ),
+                      const KeyboardCard(
+                        valeurKey: "+/-",
+                        couleurKey: grisChiffre,
+                      ),
                     ],
                   ),
                   // Deuxième Colonne de boutons
                   Column(
                     children: [
                       // % (Pourcentage)
-                      const KeyboardCard(valeurKey: "%"),
+                      KeyboardCard(
+                        valeurKey: "%",
+                        couleurKey: grisPrincipal,
+                        onPressed: () {
+                          onButtonTap("%");
+                        },
+                      ),
                       // Chiffres
-                      const KeyboardCard(valeurKey: "8"),
-                      const KeyboardCard(valeurKey: "5"),
-                      const KeyboardCard(valeurKey: "2"),
-                      const KeyboardCard(valeurKey: "0"),
+                      KeyboardCard(
+                        valeurKey: "8",
+                        couleurKey: grisChiffre,
+                        onPressed: () {
+                          onButtonTap("8");
+                        },
+                      ),
+                      KeyboardCard(
+                        valeurKey: "5",
+                        couleurKey: grisChiffre,
+                        onPressed: () {
+                          onButtonTap("5");
+                        },
+                      ),
+                      KeyboardCard(
+                        valeurKey: "2",
+                        couleurKey: grisChiffre,
+                        onPressed: () {
+                          onButtonTap("2");
+                        },
+                      ),
+                      KeyboardCard(
+                        valeurKey: "0",
+                        couleurKey: grisChiffre,
+                        onPressed: () {
+                          onButtonTap("0");
+                        },
+                      ),
                     ],
                   ),
                   // Troisième Colonne de boutons
                   Column(
                     children: [
                       // ÷ (Division)
-                      const KeyboardCard(valeurKey: "÷"),
+                      KeyboardCard(
+                        valeurKey: "÷",
+                        couleurKey: grisPrincipal,
+                        onPressed: () {
+                          onButtonTap("÷");
+                        },
+                      ),
                       // Chiffres et . (Point décimal)
-                      const KeyboardCard(valeurKey: "9"),
-                      const KeyboardCard(valeurKey: "6"),
-                      const KeyboardCard(valeurKey: "3"),
-                      const KeyboardCard(valeurKey: "."),
+                      KeyboardCard(
+                        valeurKey: "9",
+                        couleurKey: grisChiffre,
+                        onPressed: () {
+                          onButtonTap("9");
+                        },
+                      ),
+                      KeyboardCard(
+                        valeurKey: "6",
+                        couleurKey: grisChiffre,
+                        onPressed: () {
+                          onButtonTap("6");
+                        },
+                      ),
+                      KeyboardCard(
+                        valeurKey: "3",
+                        couleurKey: grisChiffre,
+                        onPressed: () {
+                          onButtonTap("3");
+                        },
+                      ),
+                      KeyboardCard(
+                        valeurKey: ".",
+                        couleurKey: grisChiffre,
+                        onPressed: () {
+                          onButtonTap(".");
+                        },
+                      ),
                     ],
                   ),
                   // Quatrième Colonne de boutons (Opérations)
                   Column(
                     children: [
                       // x (Multiplication)
-                      const KeyboardCard(valeurKey: "x"),
+                      KeyboardCard(
+                        valeurKey: "x",
+                        couleurKey: grisPrincipal,
+                        onPressed: () {
+                          onButtonTap("x");
+                        },
+                      ),
                       // - (Soustraction)
-                      const KeyboardCard(valeurKey: "-"),
+                      KeyboardCard(
+                        valeurKey: "-",
+                        couleurKey: grisPrincipal,
+                        onPressed: () {
+                          onButtonTap("-");
+                        },
+                      ),
                       // + (Addition)
-                      const KeyboardCard(valeurKey: "+"),
+                      KeyboardCard(
+                        valeurKey: "+",
+                        couleurKey: grisPrincipal,
+                        onPressed: () {
+                          onButtonTap("+");
+                        },
+                      ),
                       // = (Égalité) - Note: Ce bouton a une tailleKey différente (plus grand).
-                      const KeyboardCard(
+                      KeyboardCard(
                         valeurKey: "=",
                         couleurKey:
                             Colors.orange, // Couleur du bouton en orange
-                        tailleKey: 165, // Taille verticale plus grande
+                        tailleKey: 165,
+                        onPressed:
+                            calculateResult, // Taille verticale plus grande
                       ),
                     ],
                   ),
@@ -163,6 +341,7 @@ class KeyboardCard extends StatelessWidget {
 
   /// Hauteur du bouton.
   final double tailleKey;
+  final VoidCallback? onPressed;
 
   /// Constructeur de la KeyboardCard avec des valeurs par défaut.
   const KeyboardCard({
@@ -173,29 +352,36 @@ class KeyboardCard extends StatelessWidget {
     this.couleurKey = Colors.black87,
     // Taille (hauteur) par défaut : 80.
     this.tailleKey = 80,
+    this.onPressed,
   });
 
   @override
   Widget build(BuildContext context) {
     // Card est un conteneur avec des coins arrondis et une élévation (ombre).
     return Card(
-      elevation: 10, // Ajoute une ombre pour un effet 3D.
-      child: Container(
-        color: couleurKey, // Utilise la couleur de fond spécifiée.
-        width: 80, // Largeur fixe pour les boutons.
-        height:
-            tailleKey, // Hauteur spécifiée (80 par défaut, ou plus pour '=').
-        // Center pour centrer le texte dans le conteneur.
-        child: Center(
-          child: Text(
-            valeurKey, // Affiche la valeur du bouton.
-            style: const TextStyle(
-              color: Colors.white,
-              fontSize: 30,
-            ), // Texte blanc de grande taille.
+      elevation: 5,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+      color: couleurKey,
+      child: InkWell(
+        onTap: onPressed,
+        borderRadius: BorderRadius.circular(24),
+        child: SizedBox(
+          width: 80, // Largeur fixe pour les boutons.
+          height:
+              tailleKey, // Hauteur spécifiée (80 par défaut, ou plus pour '=').
+          // Center pour centrer le texte dans le conteneur.
+          child: Center(
+            child: Text(
+              valeurKey, // Affiche la valeur du bouton.
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ), // Texte blanc de grande taille.
+            ),
           ),
         ),
-      ),
+      ), // Ajoute une ombre pour un effet 3D.
     );
   }
 }
